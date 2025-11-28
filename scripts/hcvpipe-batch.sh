de72a3b263d48763bdd28b4d26c4aea3cdd71704
Jonas Björkman <jonas@cloud8.se>
2025-11-28 15:56:19 +0100

    major recoding

--- scripts/hcvpipe-batch.sh
+++ scripts/hcvpipe-batch.sh
@@ -5,6 +5,9 @@ runname=$(basename $fulldir)
 subsample=1000000
 outdir=/fs1/jonas/hcv/results/
 logdir=/fs1/jonas/hcv/logs/
+partition=low
+dry=''
+[[ "$2" == 'dry' ]] && dry='echo'
 
 if [[ ! -d $fulldir ]] ; then
 	echo $fulldir is not a directory
@@ -16,7 +19,7 @@ cd $logdir
 for sample in ${fulldir}/*R1*gz ; do
 	jobname=$(basename $sample)
 	jobname=${jobname%%_*}
-	sbatch -J HCV-${jobname} $(dirname $0)/hcvpipe.sh -f -s $subsample -o ${outdir}/${runname} $sample
+	$dry sbatch -J HCV-${jobname} --partition $partition $(dirname $0)/hcvpipe.sh -f -s $subsample -o ${outdir}/${runname} $sample
 done
 
 cd - > /dev/null
